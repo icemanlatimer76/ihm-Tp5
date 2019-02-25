1413f3435371cd39f181318f5df21f6b5eb358bf
icemanlatimer76 <31551262+icemanlatimer76@users.noreply.github.com>
2019-02-25 09:47:44 +0100

    TE

--- Conv.java
+++ Conv.java
@@ -1,30 +1,94 @@
-import javafx.scene.paint.Color;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Conv extends Frame implements ActionListener, ItemListener {
-    private static final double EURO_VERS_FRANCS = 6.55957;
-    private static final Color BLEU_FONCE = Color.BLUEVIOLET;
-    private static final Color BLEU_CLAIR = Color.BLUE;
+  private static final double EURO_VERS_FRANCS = 6.55957;
+  private static final Color BLEU_FONCE = new Color(111, 130, 191);
+  private static final Color BLEU_CLAIR = new Color(130, 160, 250);
 
-    private CheckboxGroup cbgCadre;
+  private CheckboxGroup cbgCadre;
 
-    private Checkbox cbFranc;
-    private Checkbox cbEuro;
-    private TextField txtSaisie;
-    private Label lblMessage;
+  private Checkbox cbFranc;
+  private Checkbox cbEuro;
+  private TextField txtSaisie;
+  private Label lblMessage1;
+  private Label lblMessage2;
 
-    public Conv() {
+  public Conv() {
 
+    setTitle("Convertiseur");
+    setLocation(300, 300);
+    setSize(1400, 1400);
+
+    setLayout(new GridLayout(7, 1)); // 2
+
+    setBackground(BLEU_FONCE);
+    cbgCadre = new CheckboxGroup();
+
+    cbEuro = new Checkbox("Euro", cbgCadre, false);
+    cbFranc = new Checkbox("Franc", cbgCadre, false);
+
+    cbEuro.addItemListener(this);
+    cbFranc.addItemListener(this);
+
+    add(new Label("Quelle monnaie voulez-vous convertir ?"));
+    add(cbFranc);
+    add(cbEuro);
+    lblMessage1 = new Label("");
+    add(lblMessage1);
+
+    add(new Label("Montant à convertir"));
+    txtSaisie = new TextField();
+    txtSaisie.addActionListener(this);
+    add(txtSaisie);
+
+    lblMessage2 = new Label("");
+    Font f = new Font("name", 10, 30);
+    lblMessage2.setFont(f);
+    add(lblMessage2);
+
+    pack();
+    setVisible(true);
+  }
+
+  public void itemStateChanged(ItemEvent e) {
+    if (cbEuro.getLabel() == e.getItem()) {
+      cbEuro.setBackground(BLEU_CLAIR);
+      cbFranc.setBackground(BLEU_FONCE);
+      lblMessage1.setText("Euro -->  Franc");
     }
 
-    public void itemStateChanged(ItemEvent e) {
-
+    if (cbFranc.getLabel() == e.getItem()) {
+      cbFranc.setBackground(BLEU_CLAIR);
+      cbEuro.setBackground(BLEU_FONCE);
+      lblMessage1.setText("Franc -->  Euro");
     }
 
-    public void actionPerformed(ActionEvent e) {
+  }
 
+  public void actionPerformed(ActionEvent e) {
+
+    double num;
+    int round;
+
+    num = Double.parseDouble(txtSaisie.getText());
+
+    if (cbgCadre.getCurrent().equals(cbFranc)) {
+      num = num / EURO_VERS_FRANCS;
+      round = (int) (num * 100);
+      num = round / 100.00;
+      lblMessage2.setText(num + " Euros");
+    }
+    if (cbgCadre.getCurrent().equals(cbEuro)) {
+      num = num * EURO_VERS_FRANCS;
+      round = (int) (num * 100);
+      num = round / 100.00;
+      lblMessage2.setText(num + " Franc");
     }
 
-    public static void main(String[] args) {
-        new Conv();
-    }
+  }
+
+  public static void main(String[] args) {
+    new Conv();
+  }
 }
--- Exo2.java
+++ Exo2.java
@@ -11,31 +11,31 @@ public class Exo2 extends Frame implements ItemListener // 1
     private Checkbox cbDessert;
 
     public Exo2() {
-        this.setTitle("Cases à cocher");
-        this.setLocation(50, 50);
+        setTitle("Cases à cocher");
+        setLocation(50, 50);
 
-        this.setLayout(new GridLayout(3, 2)); // 2
-        this.cbgCadre = new CheckboxGroup(); // 4
+        setLayout(new GridLayout(3, 2)); // 2
+        cbgCadre = new CheckboxGroup(); // 4
         // Ajout de choix NON exclusif
-        this.add(new Label("Choix Non exclusif"));
-        this.add(new Label("Choix exclusif"));
+        add(new Label("Choix Non exclusif"));
+        add(new Label("Choix exclusif"));
 
-        this.cbEntree = new Checkbox("Entrée");
-        this.cbFromage = new Checkbox("Fromage", this.cbgCadre, false);
-        this.cbPlat = new Checkbox("Plat");
-        this.cbDessert = new Checkbox("Dessert", this.cbgCadre, false);
+        cbEntree = new Checkbox("Entrée");
+        cbFromage = new Checkbox("Fromage", cbgCadre, false);
+        cbPlat = new Checkbox("Plat");
+        cbDessert = new Checkbox("Dessert", cbgCadre, false);
 
-        this.cbEntree.addItemListener(this); // 3
-        this.cbPlat.addItemListener(this);
-        this.cbFromage.addItemListener(this);
-        this.cbDessert.addItemListener(this);
-        this.add(this.cbEntree);
-        this.add(this.cbFromage);
-        this.add(this.cbPlat);
-        this.add(this.cbDessert);
+        cbEntree.addItemListener(this); // 3
+        cbPlat.addItemListener(this);
+        cbFromage.addItemListener(this);
+        cbDessert.addItemListener(this);
+        add(cbEntree);
+        add(cbFromage);
+        add(cbPlat);
+        add(cbDessert);
 
-        this.pack();
-        this.setVisible(true);
+        pack();
+        setVisible(true);
     }
 
     public void itemStateChanged(ItemEvent e) // 5
